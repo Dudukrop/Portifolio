b95d68a8d137b3078236acbc740dfd9e257789a0
Eduardo Kropniczki <dudukrop@gmail.com>
2024-04-26 23:55:47 -0300

    responsividade para pagina sobre

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,13 +3,13 @@ import Span from "./span/Span";
 export default function About() {
 	return (
 		<div id="about" className="scrollAlign w-full flex items-center h-screen">
-			<div className="flex flex-col-reverse lg:flex-row items-center 
+			<div className="flex flex-col lg:flex-row items-center 
             m-auto relative bottom-[4vh] lg:top-8 w-[92%] lg:w-[98%] justify-center">
-				<div className="flex flex-col items-center lg:items-center justify-between w-full 
+				<div className="mt-10 lg:mt-0 flex flex-col items-center lg:items-center justify-between w-full 
                 lg:w-[350px] lg:h-[350px] lg:mr-10 h-[300px] rounded-3xl overflow-hidden">
 					<img src="Perfil.jpg" className="w-[350px]" />
 				</div>
-				<div className="h-[300px] flex flex-col justify-around lg:w-[600px] w-[90vw] z-50">
+				<div className="h-[350px] lg:h-[300px] mt-10 lg:mt-0 flex flex-col justify-around lg:w-[600px] w-[90vw] z-50">
 					<div>
 						<span className="text-2xl text-gray-300">
 							Dev Full Stack / .Net / C# / React / Azure
