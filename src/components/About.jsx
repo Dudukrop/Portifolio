921147ba5bff1e17f753508f4fc78492b35b534d
Eduardo Kropniczki <dudukrop@gmail.com>
2024-04-29 16:49:14 -0300

    responsividade ajustada

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,12 @@ export default function About() {
 		<div id="about" className="scrollAlign w-full flex items-center h-screen">
 			<div className="flex flex-col md:flex-row items-center 
             m-auto relative h-[680px] md:h-auto bottom-[2vh] md:top-8 w-[92%] md:w-[98%] justify-center md:justify-around">
-				<div className="md:mt-0 flex flex-col items-center md:items-center justify-between w-[200px] md:w-[400px] rounded-full overflow-hidden">
-					<img src="FotoPerfil.jpg" className="w-[400px]" />
+				<div>
+					<div className="md:mt-0 mt-5 flex flex-col items-center md:items-center justify-between w-[220px] md:w-[400px] rounded-full overflow-hidden">
+						<img src="FotoPerfil.jpg" className="md:w-[400px]" />
+					</div>
 				</div>
-				<div className="h-[450px] relative bottom-5 md:bottom-2 md:h-[420px] mt-10 md:mt-0 flex flex-col justify-between md:w-[600px] w-[90vw] z-50">
+				<div className="h-[450px] relative bottom-5 md:bottom-2 md:h-[420px] mt-5 md:mt-0 flex flex-col justify-between md:w-[600px] w-[90vw] z-50">
 					<div className="h-[120px] relative top-5 md:top-0 md:h-[27vh] md:w-[38vw]">
 						<Title animation="bounce">
 							Olá, Eu Sou
@@ -20,22 +22,23 @@ export default function About() {
 					</div>
 					<div>
 						<span className="md:text-2xl text-xl text-[#9c27b0] ">
-							Dev Full Stack / .Net / C# / React / Azure
+							Desenvolvedor Full Stack / Freelancer / Web
 						</span>
 					</div>
 					<div>
 						<Span>
-							Profissionalmente conectado à indústria de desenvolvimento web.
+							Entusiasta da programação, mergulhado na jornada do aprendizado constante.
 						</Span>
 					</div>
 					<div>
 						<Span>
-							Solução de problemas, organizado, funcionário leal com grande atenção aos detalhes. Fã de academia, atividades ao ar livre, videogames e, é claro, codificação.
+							Sempre pronto para liderar e compartilhar conhecimento. Amante de academia e atividades ao ar livre, perfeccionista quando se trata de programação.
 						</Span>
 					</div>
 					<div>
 						<Span>
-							Interessado em todo o espectro de desenvolvimento web e trabalhando em projetos ambiciosos com pessoas interessantes.
+							Interessado em todo o espectro de desenvolvimento web,
+							encontrando alegria e satisfação em cada projeto.
 						</Span>
 					</div>
 				</div>
